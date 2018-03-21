7b5a3e071f6559cb192c260f32771123e1952eef
Rafie Gilang <gilang0293@gmail.com>
2018-03-21 15:27:03 +0700

    sudah selesai promis-countdown

--- views.js
+++ views.js
@@ -4,8 +4,22 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
+  //saya bikin prosses convert disini biar enggak otak atik parameter
+  let min = 0;
+  let secs = 0;
+  if(seconds >= 60){
+    min = Math.floor(seconds/60);
+    sec = seconds % 60;
+  }else{
+    sec = seconds;
+  }
+
+  if(min < 10) min = '0'+min;
+  if(sec < 10) sec = '0'+sec;
+
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.green(figlet.textSync(`${min} : ${sec} `)));
+
 };
 
 module.exports = { displayCountdown };
