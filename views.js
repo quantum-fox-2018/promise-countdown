1d46758561ec6be0e5885abcefbeee5e60b13584
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-21 14:08:20 +0700

    promise countdown

--- views.js
+++ views.js
@@ -5,7 +5,18 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  let minute = Math.floor(seconds/60);
+  let second = seconds%60;
+  
+  if(minute <= 9 && second <= 9){
+    console.log(chalk.blue(figlet.textSync(`0${minute}:0${second}`)))
+  } else if(minute <= 9){
+    console.log(chalk.blue(figlet.textSync(`0${minute}:${second}`)))
+  } else if(second <= 9){
+    console.log(chalk.blue(figlet.textSync(`${minute}:0${second}`)))
+  } else {
+    console.log(chalk.blue(figlet.textSync(`${minute}:${second}`)))
+  }
 };
 
 module.exports = { displayCountdown };
