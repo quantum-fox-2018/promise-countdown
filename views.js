5406c5b4b3c13745f9d0999390af8aab02a43498
Natascha Lamsu <tazcha15@gmail.com>
2018-03-21 18:10:07 +0700

    promise countdown clear

--- views.js
+++ views.js
@@ -6,6 +6,19 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit = Math.floor(seconds/60) + '';
+  let detik = seconds%60 + '';
+
+  if(menit.length < 2) {
+    var minute = `0${menit}`
+  }
+
+  if(detik.length < 2) {
+    var sec = `0${detik}`
+  }
+
+  let countdown = `${minute}:${sec}`
+  console.log(figlet.textSync(countdown));
 };
 
 module.exports = { displayCountdown };
